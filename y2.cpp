bb3bed7afbc93a831078e2a4b49f4b3139cad52b
stoopkid <bdavis2347@gmail.com>
2016-11-17 23:32:40 -0500

    is this paradoxical?

--- y2.cpp
+++ y2.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 /*
 References:
+"A Lecture on the 'Why' of 'Y'" by Matthias Felleisen
+http://www.ps.uni-sb.de/courses/sem-prog97/material/YYWorks.ps
+
 https://yongweiwu.wordpress.com/2014/12/14/y-combinator-and-cplusplus/#fn-39-10
 
 http://mvanier.livejournal.com/2897.html
@@ -12,6 +15,8 @@ http://mvanier.livejournal.com/2897.html
 
 */
 
+
+//Standard factorial function
 int factorial(int n){
  if (n == 0) return 1;
  else return n * factorial(n - 1);
@@ -28,6 +33,42 @@ template <typename F>
 struct self_ref_func {
   function<F(self_ref_func<F>)> fn;
 };
+
+/*
+Assume
+T : Type
+self_ref_func<T> A;
+
+then
+A.fn : function<T(self_ref_func<T>)>
+
+what can you call this function on?
+you need another self_ref_func<T>
+
+so assume:
+self_ref_func B;
+
+now you can call A.fn(B).
+But also:
+B.fn : function<T(self_ref_func<T>)>
+
+what can you call B.fn on?
+You can't keep creating new 
+self_ref_func B2;
+self_ref_func_B3;
+...
+forever
+
+but you can call B.fn on A, because
+A : self_ref_func<T>
+
+and you can also call A.fn on A, because
+A.fn : function<T(self_ref_func<T>)>
+
+A.fn(A) simulates the untypable (x x)
+
+
+*/
 
 typedef function<int(int)> fn_1ord;
 typedef function<fn_1ord(fn_1ord)> fn_2ord;
@@ -53,6 +94,16 @@ function<B(A)> Y(function<function<B(A)>(function<B(A)>)> f)
  return f([=](A x) {return Y(f)(x);});
 }
 
+template <typename A, typename B>
+function<B(A)> Y_comb(function<function<B(A)>(function<B(A)>)> f)
+{
+ fn_self_ref r;
+ r.fn = [f](fn_self_ref x){
+  return f ([x](A y){return x.fn(x)(y);});
+ };
+ return r.fn(r);
+}
+
 int main() {
  //Oops, segmentation fault!
  //function<int(int)> fac_inf = Y_inf(almost_factorial);
@@ -64,9 +115,11 @@ int main() {
  function<int(int)> fac1 = almost_factorial(almost_factorial(identity));
  function<int(int)> fac2 = almost_factorial(almost_factorial(almost_factorial(identity)));
  function<int(int)> fac3 = almost_factorial(almost_factorial(almost_factorial(almost_factorial(identity))));
+ function<int(int)> facY = Y_comb(almost_factorial);
  cout << "factorial(10) = " << factorial(10) << endl;
  cout << "fac(10) = " << fac(10) << endl;
  cout << "facA(10) = " << facA(10) << endl;
+ cout << "facY(10) = " << facY(10) << endl;
  cout << "fac0(0) = " << fac0(0) << endl;
  cout << "fac0(1) = " << fac0(1) << endl;
  cout << "fac1(0) = " << fac1(0) << endl;
