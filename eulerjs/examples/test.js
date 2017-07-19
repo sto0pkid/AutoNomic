1ba5f15312c4f059511d7c2c70bdb1431c6b0f04
stoopkid <stoopkid@stoopkid.com>
2017-07-18 21:53:17 -0400

    yet another euler.js bug

---
+++ eulerjs/examples/test.js
@@ -0,0 +1,22 @@
+var cases = {
+  "GND":{},
+  "p":[
+	{
+	 head:{pred: "p", args:[{pred: ":A", args:[]}, {pred: ":B", args:[]}]}, 
+	 body: []
+	},
+	{
+	 head:{pred: "p", args:[{pred: ":B", args:[]}, {pred: ":C", args:[]}]}, 
+	 body: []
+	},
+	{
+	 head:{pred: "p", args:[{pred: ":C", args:[]}, {pred: ":D", args:[]}]}, 
+	 body: []
+	}
+
+],
+  "":[
+    {head:{}, body:[{pred:"p", args:[{pred:"?what", args:[]}, {pred:"?what", args:[]}]}]}
+  ]
+}
+
